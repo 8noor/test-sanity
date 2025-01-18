8a1cb97e4de6ee8ae6ccf7cca08bf13c03733b1d
NOORANUM <nanum3773@gmail.com>
2025-01-18 07:27:59 +0500

    first commit

---
+++ next-sanity-blog/sanity/lib/demo.ts
@@ -0,0 +1,31 @@
+/**
+ * Demo data used as placeholders and initial values for the blog
+ */
+
+export const title = "Animals And Birds Blog.";
+
+export const description = [
+  {
+    _key: "9f1a629887fd",
+    _type: "block",
+    children: [
+      {
+        _key: "4a58edd077880",
+        _type: "span",
+        marks: [],
+        text: "Welcome to the blog.",
+      },
+      {
+        _key: "4a58edd077881",
+        _type: "span",
+        marks: [],
+        text: "Anum Rajput",
+      },
+    
+    ],
+    
+    style: "normal",
+  },
+];
+
+export const ogImageTitle = "A Next.js Blog with a Native Authoring Experience";
